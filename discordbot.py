2060386bda5bcfaab0802536f2723dd5fe83e464
pack-123 <68808315+pack-123@users.noreply.github.com>
2020-07-26 19:00:24 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,15 +7,19 @@ token = os.environ['DISCORD_BOT_TOKEN']
 
 
 @bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+async def on_message(message):
+    if message.content.startswith('!test'):
+        counter = 0
+        tmp = await client.send_message(message.channel, 'Calculating messages...')
+        async for log in client.logs_from(message.channel, limit=100):
+            if log.author == message.author:
+                counter += 1
 
+        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+    elif message.content.startswith('!sleep'):
+        await asyncio.sleep(5)
+        await client.send_message(message.channel, 'Done sleeping')
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('abc')
 
 
 bot.run(token)
